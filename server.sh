d9911f98a493284f43dd47ceba1404981ec711b0
xiihaiqal <71058792+xiihaiqal@users.noreply.github.com>
2020-10-28 02:13:38 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,18 +1,5 @@
 MYIP=$(wget -qO- ipv4.icanhazip.com);
-: '
-# check registered ip
-wget -q -O daftarip http://188.166.215.119:85/ocs/ip.txt
-if ! grep -w -q $MYIP daftarip; then
-	echo "Sorry, only registered IPs can use this script!"
-	if [[ $vps = "vps" ]]; then
-		echo "Modified by Klai"
-	else
-		echo "Modified by Klai"
-	fi
-	rm -f /root/daftarip
-	exit
-fi
-'
+
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
